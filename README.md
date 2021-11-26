dc4e590da97b4f97312234544e89144f53cd6b2f
Your Name <you@example.com>
2021-11-26 15:15:21 +0000

    validation feature added

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ This package uses pcap package to listen on default interface and port 443 to de
 ## Purphose
 imagine you have purchased a domainname and you do not know if this domain has traffic and if so you want to know if you receive https traffic and if so you need to know which subdomain you need to set-up. This small tool will help you seeing all SNI Extention handshakes of TLS.
 
+## Validation
+Added SSL Validation for seen hosts. Every 10minutes we do check Domains for SSL certificates and mark them with 'SSL VERIFIED' if verified, otherwise it returns the error code as Red. 
+
 ## Requirements
+For ubuntu:
 `apt install build-essential libpcap-dev`
--- sni-capture.js
+++ sni-capture.js
@@ -1,50 +1,122 @@
-var pcap = require('pcap'),
+const chalk = require('chalk')
+const https = require('https')
+
+const pcap = require('pcap'),
     tcp_tracker = new pcap.TCPTracker(),
     pcap_session = pcap.createSession('', { filter: "dst port 443" });
 
 tcp_tracker.on('session', function (session) {
-//   console.log("Start of session between " + session.src_name + " and " + session.dst_name);
-  session.on('end', function (session) {
-    //   console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
-  });
+    //   console.log("Start of session between " + session.src_name + " and " + session.dst_name);
+    session.on('end', function (session) {
+        //   console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
+    });
 });
 
 function getSNI(buf) {
     // https://stackoverflow.com/questions/17832592/extract-server-name-indication-sni-from-tls-client-hello?newreg=e81bc029b51841e5a793f2babc9da27f
     var sni = null
-      , regex = /^(?:[a-z0-9-]+\.)+[a-z]+$/i;
-    for(var b = 0, prev, start, end, str; b < buf.length; b++) {
-      if(prev === 0 && buf[b] === 0) {
-        start = b + 2;
-        end   = start + buf[b + 1];
-        if(start < end && end < buf.length) {
-          str = buf.toString("utf8", start, end);
-          if(regex.test(str)) {
-            sni = str;
-            continue;
-          }
+        , regex = /^(?:[a-z0-9-]+\.)+[a-z]+$/i;
+    for (var b = 0, prev, start, end, str; b < buf.length; b++) {
+        if (prev === 0 && buf[b] === 0) {
+            start = b + 2;
+            end = start + buf[b + 1];
+            if (start < end && end < buf.length) {
+                str = buf.toString("utf8", start, end);
+                if (regex.test(str)) {
+                    sni = str.toLowerCase();
+                    continue;
+                }
+            }
         }
-      }
-      prev = buf[b];
+        prev = buf[b];
     }
     return sni;
-  }
-  
+}
 
-pcap_session.on('packet', function (raw_packet) {
-    var packet = pcap.decode.packet(raw_packet);
-    let {data} = packet.payload.payload.payload
-    if (data )
-        {
-            var x = getSNI(data)
+
+function checkHttps(host) {
+    return new Promise((resolve, reject) => {
+        var options = {
+            host: host,
+            port: 443,
+            method: 'GET',
+            rejectUnauthorized: false
+        };
+
+        var req = https.request(options, function (res) {
+            var certificateInfo = res.connection.getPeerCertificate();
+            var dateInfo = {
+                valid_from: new Date(certificateInfo.valid_from),
+                valid_to: new Date(certificateInfo.valid_to)
+            };
+            resolve(dateInfo);
+        });
+        req.on('error', (err) => {
+            reject(err);
+        })
+        req.end();
+    })
+}
+
+let hostname_data = {
+
+}
+
+pcap_session.on('packet', async function (raw_packet) {
+    try {
+        let packet = pcap.decode.packet(raw_packet);
+        let { data } = packet.payload.payload.payload
+        if (data) {
+            var sniHostname = getSNI(data)
             var daddr = packet.payload.payload.daddr.toString()
             var saddr = packet.payload.payload.saddr.toString()
             var dport = packet.payload.payload.payload.dport
-            if (x) 
-                console.log(`SNI Captured: ${saddr} -> ${daddr}:${dport} ${x}`)
-            
+            if (sniHostname) {
+                
+                if (!hostname_data.hasOwnProperty(sniHostname)) {
+                    hostname_data[sniHostname] = {seen:0}
+                }
+
+                if (!hostname_data[sniHostname].t) {
+                    try {
+                        let ssl_info = await checkHttps(sniHostname)
+                        // console.log(ssl_info)
+                        if (ssl_info && ssl_info.valid_to && Date.now() < ssl_info.valid_to) {
+                            hostname_data[sniHostname].t = Date.now()
+                            hostname_data[sniHostname].state = 'SSL'
+                            hostname_data[sniHostname].details = chalk.green('SSL VERIFIED')
+                        }
+                    } catch (_err) {
+                        // errored
+                        hostname_data[sniHostname].t = Date.now()
+                        hostname_data[sniHostname].state = 'ERROR'
+                        hostname_data[sniHostname].details = chalk.red(_err.errno)
+                        // console.log(_err)
+                    }
+                } 
+                
+                if (hostname_data[sniHostname].state==='ERROR' && Date.now()-hostname_data[sniHostname].t>10*60*1000) {
+                    // every 10 minute we can reset ERROR ed checks
+                    console.log(`Reset Error of ${sniHostname}`)
+                    delete hostname_data[sniHostname]
+                    return
+                }
+                hostname_data[sniHostname].seen++
+                
+                    
+
+                console.log(`SNI: ${saddr} -> ${daddr}:${dport} ${chalk.yellow(sniHostname)} (${hostname_data[sniHostname].details}) seen:${hostname_data[sniHostname].seen}`)
+
+            }
+
+
         }
-    tcp_tracker.track_packet(packet);
+    } catch (err2) {
+        console.log(err2)
+    }
+
+
+    // tcp_tracker.track_packet(packet);
 });
 
 
